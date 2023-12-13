8157d14053237cdc49f31cb3984f772f2807173a
mistWil <7448@holbertonstudents.com>
2023-12-13 09:09:30 -0800

    simple_shell.c

--- simple_shell.c
+++ simple_shell.c
@@ -4,66 +4,52 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <string.h>
-
 #define MAX_BUFFER_SIZE 1024
 /**
  * main - simple_shell
  *
  * Return: int
  */
-
-int main()
+int main(void)
 {
 	char *command = NULL;
 	size_t bufferSize = 0;
 
-	while (1) {
-		/*Afficher l'invite*/
+	while (1)
+	{
 		printf("$ ");
 
-		/*Lire la commande de l'utilisateur avec getline*/
 		ssize_t nread = getline(&command, &bufferSize, stdin);
 
 		if (nread == -1)
 		{
-			/*Gérer la fin de fichier (Ctrl+D)*/
 			printf("\n");
 			free(command);
 			break;
 		}
 
-		/*Supprimer le caractère de nouvelle ligne à la fin de la commande*/
 		command[strcspn(command, "\n")] = '\0';
-
-		/*Créer un processus fils*/
 		pid_t pid = fork();
 
 		if (pid == -1)
 		{
-			/*Gérer les erreurs de fork*/
 			perror("fork");
 			free(command);
 			exit(EXIT_FAILURE);
 		}
 		else if (pid == 0)
 		{
-			/*Code du processus fils*/
-			/*Exécuter la commande avec execve*/
 			char *args[] = {command, NULL};
-			execve(command, args, NULL);
 
-			/*Si execve échoue, afficher un message d'erreur*/
+			execve(command, args, NULL);
 			perror("execve");
 			free(command);
 			exit(EXIT_FAILURE);
 		}
 		else
 		{
-			/*Code du processus parent*/
-			/*Attendre que le processus fils se termine*/
 			wait(NULL);
 		}
 	}
-
 	return (0);
 }
