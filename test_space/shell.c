90d6ed41f250bf65ce1287874b5da22e758bde03
Julieed-971 <julie.dedieu1@gmail.com>
2023-12-14 08:10:00 -0800

    Added interactive mode

--- test_space/shell.c
+++ test_space/shell.c
@@ -19,34 +19,33 @@ int main(void)
 
 	while (1)
 	{
-		printf("$ ");
-		bytesRead = getline(&path, &pathSize, stdin);
-		if (bytesRead == -1)
+		if (isatty(STDIN_FILENO) == 1)
 		{
-			perror("Error");
-			if (feof(stdin))
-				break;
-		}
-		path[bytesRead - 1] = '\0';
-
-		childPid = fork();
-		if (childPid == -1)
-		{
-			perror("Error");
-			exit(EXIT_FAILURE);
-		}
-		else if (childPid == 0)
-		{
-			if (execve(path, args, environ) == -1)
+			printf("$ ");
+			bytesRead = getline(&path, &pathSize, stdin);
+			if (bytesRead == -1)
 			{
 				perror("Error");
-				free(path);
-				exit(EXIT_FAILURE);
+				if (feof(stdin))
+					free(path),	exit(EXIT_SUCCESS);
 			}
-		}
-		else
+			path[bytesRead - 1] = '\0';
+
+			childPid = fork();
+			if (childPid == -1)
+				perror("Error"), exit(EXIT_FAILURE);
+			else if (childPid == 0)
+			{
+				if (execve(path, args, environ) == -1)
+				{
+					fprintf(stderr, "%s: command not found\n", path);
+					free(path);
+					exit(EXIT_FAILURE);
+				}
+			}
 			wait(&status);
+		}
+		free(path);
+		return (0);
 	}
-	free(path);
-	return (0);
 }
