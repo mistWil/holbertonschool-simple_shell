f6d87bc1f44c7fbba58c72a5a3a8878b62cf336a
Julieed-971 <julie.dedieu1@gmail.com>
2023-12-15 00:24:43 -0800

    Changed isatty

--- shell.c
+++ shell.c
@@ -22,30 +22,30 @@ int main(void)
 		if (isatty(STDIN_FILENO) == 1)
 		{
 			printf("$ ");
-			bytesRead = getline(&path, &pathSize, stdin);
-			if (bytesRead == -1)
-			{
-				perror("Error");
-				if (feof(stdin))
-					free(path),	exit(EXIT_SUCCESS);
-			}
-			path[bytesRead - 1] = '\0';
-
-			childPid = fork();
-			if (childPid == -1)
-				perror("Error"), exit(EXIT_FAILURE);
-			else if (childPid == 0)
-			{
-				if (execve(path, args, environ) == -1)
-				{
-					fprintf(stderr, "%s: command not found\n", path);
-					free(path);
-					exit(EXIT_FAILURE);
-				}
-			}
-			wait(&status);
 		}
-		free(path);
-		return (0);
+		bytesRead = getline(&path, &pathSize, stdin);
+		if (bytesRead == -1)
+		{
+			perror("Error");
+			if (feof(stdin))
+				free(path),	exit(EXIT_SUCCESS);
+		}
+		path[bytesRead - 1] = '\0';
+
+		childPid = fork();
+		if (childPid == -1)
+			perror("Error"), exit(EXIT_FAILURE);
+		else if (childPid == 0)
+		{
+			if (execve(path, args, environ) == -1)
+			{
+				fprintf(stderr, "%s: command not found\n", path);
+				free(path);
+				exit(EXIT_FAILURE);
+			}
+		}
+		wait(&status);
 	}
+	free(path);
+	return (0);
 }
