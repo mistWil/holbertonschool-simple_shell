76115ee6ce3b01b18e5d49947048c21fc2fbe439
Julieed-971 <julie.dedieu1@gmail.com>
2023-12-19 07:11:56 -0800

    Header file for new shell

---
+++ new_shell.h
@@ -0,0 +1,21 @@
+#ifndef NEW_SHELL_H
+#define NEW_SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#define MAX_INPUT_SIZE 1024
+
+extern char **environ;
+
+int main(void);
+char *read_line(void);
+int execute_command(char **args);
+
+#endif
