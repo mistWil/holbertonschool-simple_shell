e5dcaaed064cca3bf62908783290879560bd24e6
Julieed-971 <julie.dedieu1@gmail.com>
2023-12-22 10:44:59 -0800

    Fix: input not read

--- shell.c
+++ shell.c
@@ -9,25 +9,18 @@
  */
 int main(void)
 {
-	char *user_input = NULL, *line = NULL;
+	char *line = NULL;
 	char **args = NULL;
-	ssize_t bytes_read;
-	size_t buff_size = 0;
 
 	while (1)
 	{
 		if (isatty(STDIN_FILENO) == 1)
-			printf("$ ");
-		fflush(stdout);
-		bytes_read = getline(&user_input, &buff_size, stdin);
-		if (bytes_read == EOF)
-			free(user_input), exit(0);
-		line = user_input;
-		if (bytes_read > 0 && line[bytes_read - 1] == '\n')
 		{
-			line[bytes_read - 1] = '\0';
-			continue;
+			printf("$ ");
 		}
+		fflush(stdout);
+
+		line = read_line();
 		args = split_line(line);
 		if (args[0] == NULL)
 		{
@@ -47,4 +40,28 @@ int main(void)
 	}
 	free(line);
 	return (0);
+}
+
+/**
+ * read_line - function to read the user inputs
+ * @void
+ *
+ * Return: char
+ */
+
+char *read_line(void)
+{
+	char *line = NULL;
+	ssize_t bytes_read;
+	size_t buff_size = 0;
+
+	bytes_read = getline(&line, &buff_size, stdin);
+	if (bytes_read == EOF)
+	{
+		free(line);
+		exit(0);
+	}
+	if (bytes_read > 0 && line[bytes_read - 1] == '\n')
+		line[bytes_read - 1] = '\0';
+	return (line);
 }
